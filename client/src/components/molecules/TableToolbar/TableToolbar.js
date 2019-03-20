8b3e3a757d5ec22d844ecf2f032f42a08332b1d3
whirish <lpoflynn@protonmail.ch>
2019-03-19 22:52:56 -0700

    WIP mui-filter integration

--- client/src/components/molecules/TableToolbar/TableToolbar.js
+++ client/src/components/molecules/TableToolbar/TableToolbar.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import compose from 'recompose/compose'
-import { withStyles } from '@material-ui/core/styles'
-import classNames from 'classnames'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 
-import Toolbar from '@material-ui/core/Toolbar'
-import Tooltip from '@material-ui/core/Tooltip'
-import IconButton from '@material-ui/core/IconButton'
-import DeleteIcon from '@material-ui/icons/Delete'
-import Typography from '@material-ui/core/Typography'
-import FilterListIcon from '@material-ui/icons/FilterList';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Typography from '@material-ui/core/Typography';
+import {
+  FilterDrawer,
+  filterSelectors,
+  filterActions
+} from 'material-ui-filter';
 
 let TableToolbar = props => {
-  const { selected, numSelected, classes } = props
-  let counter = 0
+  const {
+    selected,
+    numSelected,
+    classes,
+    setFilterIsOpen,
+    list,
+    setSearch
+  } = props;
+  let counter = 0;
+  const filterFields = [
+    { name: 'name', label: 'Name' },
+    { name: 'email', label: 'Email' },
+    { name: 'registered', label: 'Registered', type: 'date' },
+    { name: 'isActive', label: 'Is Active', type: 'bool' },
+    { name: 'testObject', label: 'Object', type: 'object' }
+  ];
+
   return (
     <Toolbar
       className={classNames(classes.root, {
-        [classes.highlight]: numSelected > 0,
+        [classes.highlight]: numSelected > 0
       })}
     >
       <div className={classes.title}>
@@ -34,19 +53,14 @@ let TableToolbar = props => {
       </div>
       <div className={classes.spacer} />
       <div className={classes.actions}>
-        {numSelected > 0 ? (
+        {numSelected > 0 && (
           <Tooltip title="Delete">
             <IconButton aria-label="Delete">
               <DeleteIcon />
             </IconButton>
           </Tooltip>
-        ) : (
-          <Tooltip title="Filter list">
-            <IconButton aria-label="Filter list">
-              <FilterListIcon />
-            </IconButton>
-          </Tooltip>
         )}
+        <FilterDrawer name="trees" fields={filterFields} />
       </div>
     </Toolbar>
   );
@@ -56,7 +70,6 @@ TableToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
   numSelected: PropTypes.number.isRequired
 };
-
 
 const styles = theme => ({
   root: {
@@ -69,29 +82,38 @@ const styles = theme => ({
   },
 
   tableWrapper: {
-    overflowX: 'auto',
+    overflowX: 'auto'
   }
-})
+});
 
 const mapState = state => {
+  const { filters } = state;
+  const { hasFilters } = filterSelectors.selectFilterProps('trees', filters);
+  const list = filterSelectors.getFilteredList('trees', filters, '');
+
   return {
     selected: state.trees.selected,
-    numSelected: state.trees.length
-  }
-}
+    numSelected: state.trees.length,
+    hasFilters,
+    list
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     dispatch
-  }
-}
+  };
+};
 
 TableToolbar.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired
-}
+};
 
 export default compose(
   withStyles(styles, { withTheme: true, name: 'TableToolbar' }),
-  connect(mapState, mapDispatch)
-)(TableToolbar)
+  connect(
+    mapState,
+    mapDispatch
+  )
+)(TableToolbar);
